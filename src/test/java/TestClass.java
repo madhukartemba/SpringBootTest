db2515bc0d2c725e7de3d9dc79815a1208135211
madhukartemba0 <madhukar.temba@arcadia.com>
2023-04-16 01:44:51 +0530

    first commit

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,13 @@
+
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+
+    @Test
+    public void contextLoads() {
+
+        System.out.println("RUNNING");
+        assert 1==2;
+    }
+
+}
